07f618648570125d16c59970a0bd63e685cdcdb3
Sam <samshum90@gmail.com>
2021-03-16 21:04:50 +0000

    added in Linq option

--- console/Prices.cs
+++ console/Prices.cs
@@ -6,12 +6,16 @@ namespace console
     public class Prices
     {
         private float? price = null;
-        private int index = -1;
+        private int day = -1;
 
         public string buyPrice(string[] stockArray)
         {
-            price = float.Parse(stockArray.Max());
-            index = Array.IndexOf(stockArray, price);
+            // Using Linq
+            var floatArray = Array.ConvertAll(stockArray, float.Parse);
+            price = floatArray.Max();
+            day = Array.IndexOf(floatArray, price) + 1;
+
+            // Using a loop
             // for (int i = 0; i < stockArray.Length; i++)
             // {
             //     if (!price.HasValue || float.Parse(stockArray[i]) > price.Value)
@@ -20,19 +24,25 @@ namespace console
             //         index = i + 1;
             //     }
             // }
-            return index.ToString() + "(" + price.ToString() + ")";
+            return day.ToString() + "(" + price.ToString() + ")";
         }
         public string sellPrice(string[] stockArray)
         {
             for (int i = 0; i < stockArray.Length; i++)
             {
-                if (!price.HasValue || float.Parse(stockArray[i]) < price.Value)
-                {
-                    price = float.Parse(stockArray[i]);
-                    index = i + 1;
-                }
+                 // Using Linq
+                var floatArray = Array.ConvertAll(stockArray, float.Parse);
+                price = floatArray.Min();
+                day = Array.IndexOf(floatArray, price) + 1;
+
+                // Using a loop
+                // if (!price.HasValue || float.Parse(stockArray[i]) < price.Value)
+                // {
+                //     price = float.Parse(stockArray[i]);
+                //     index = i + 1;
+                // }
             }
-            return index.ToString() + "(" + price.ToString() + ")";
+            return day.ToString() + "(" + price.ToString() + ")";
         }
     }
 }
